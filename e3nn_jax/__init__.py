910e577a50c68ede78c1d98c0ebb4dd5fbeadd5c
Mario Geiger <geiger.mario@gmail.com>
2021-07-07 00:36:17 +0200

    spherical harmonics

--- e3nn_jax/__init__.py
+++ e3nn_jax/__init__.py
@@ -2,12 +2,14 @@ __version__ = "0.3.1"
 
 from ._wigner import wigner_3j, wigner_D
 from ._irreps import Irrep, Irreps
+from ._spherical_harmonics import spherical_harmonics
 from ._soft_one_hot_linspace import sus, soft_one_hot_linspace
 from ._tensor_product import tensor_product, fully_connected_tensor_product
 
 __all__ = [
     "wigner_3j", "wigner_D",
     "Irrep", "Irreps",
+    "spherical_harmonics",
     "sus", "soft_one_hot_linspace",
     "tensor_product", "fully_connected_tensor_product",
 ]
