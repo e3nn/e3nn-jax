0f67fbcb2339f891677cb91476145538ccd8eac0
Mario Geiger <geiger.mario@gmail.com>
2021-10-07 16:18:59 +0200

    remove zero, it does not work

--- e3nn_jax/__init__.py
+++ e3nn_jax/__init__.py
@@ -1,6 +1,5 @@
 __version__ = "0.3.1"
 
-from ._zero import Zero, zeinsum
 from ._graph_util import index_add, radius_graph
 from ._rotation import (
     rand_matrix,
@@ -42,7 +41,6 @@ from ._activation import ScalarActivation, normalize_function
 from ._gate import Gate
 
 __all__ = [
-    "Zero", "zeinsum",
     "index_add", "radius_graph",
     "rand_matrix",
     "identity_angles",
