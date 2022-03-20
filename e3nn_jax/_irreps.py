3c3e356dcccc5d5ecdc9395698764e9059c9c682
Mario Geiger <geiger.mario@gmail.com>
2022-03-20 16:56:35 -0400

    fix

--- e3nn_jax/_irreps.py
+++ e3nn_jax/_irreps.py
@@ -995,6 +995,7 @@ class IrrepsData:
         assert {x.irreps for x in args} == {args[0].irreps}
         while axis < 0:
             axis += len(args[0].shape)
+        args = [x.replace_none_with_zeros() for x in args]
         return IrrepsData(
             args[0].irreps,
             jnp.concatenate([x.contiguous for x in args], axis=axis),
