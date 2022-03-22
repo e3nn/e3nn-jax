96d3d13e9720fe4395fc41dedf2b80566cfd9c5b
Mario Geiger <geiger.mario@gmail.com>
2022-03-22 18:00:46 -0400

    simple gate function

--- e3nn_jax/_gate.py
+++ e3nn_jax/_gate.py
@@ -1,11 +1,13 @@
 from typing import Callable, List, Optional
 
+import jax
+
 from e3nn_jax import IrrepsData, elementwise_tensor_product, scalar_activation
 from e3nn_jax.util.decorators import overload_for_irreps_without_data
 
 
 @overload_for_irreps_without_data((0,))
-def gate(input: IrrepsData, acts: List[Optional[Callable]]) -> IrrepsData:
+def gate(input: IrrepsData, acts: List[Optional[Callable]] = None, even_act=jax.nn.gelu, odd_act=jax.nn.tanh, even_gate_act=jax.nn.sigmoid, odd_gate_act=jax.nn.tanh) -> IrrepsData:
     r"""Gate activation function.
 
     The input is split into scalars that are activated separately, scalars that are used as gates, and non-scalars that are multiplied by the gates.
@@ -24,17 +26,29 @@ def gate(input: IrrepsData, acts: List[Optional[Callable]]) -> IrrepsData:
     """
     assert isinstance(input, IrrepsData)
 
-    input = scalar_activation(input, acts + [None] * (len(input.irreps) - len(acts)))
+    if acts is not None:
+        j = len(acts)
+    else:
+        j = 0
+        for j, (_, ir) in enumerate(input.irreps):
+            if not ir.is_scalar:
+                break
+    scalars, gated = input.split([j])
 
-    scalars = None
-    j = len(acts)
-    irreps_gated = input.irreps[j:]
+    if acts is not None:
+        scalars = scalar_activation(scalars, acts)
+
+    gates = None
     for i in range(j + 1):
-        if input.irreps[i:j].num_irreps == irreps_gated.num_irreps:
-            scalars, gates, gated = input.split([i, j])
+        if scalars.irreps[i:].num_irreps == gated.irreps.num_irreps:
+            scalars, gates = scalars.split([i])
             break
 
-    if scalars is None:
+    if gates is None:
         raise ValueError(f"Gate: did not manage to split the input {input.irreps} into scalars, gates and gated.")
+
+    if acts is None:
+        scalars = scalar_activation(scalars, [even_act if ir.p == 1 else odd_act for _, ir in scalars.irreps])
+        gates = scalar_activation(gates, [even_gate_act if ir.p == 1 else odd_gate_act for _, ir in gates.irreps])
 
     return IrrepsData.cat([scalars, elementwise_tensor_product(gates, gated)])
