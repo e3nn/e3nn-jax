efdf95d985a5a8a692bc86d23edfea6237c470fd
Mario Geiger <geiger.mario@gmail.com>
2022-03-27 19:34:40 -0400

    cooler code for tetris point

--- examples/tetris_point.py
+++ examples/tetris_point.py
@@ -4,8 +4,7 @@ import haiku as hk
 import jax
 import jax.numpy as jnp
 import optax
-from e3nn_jax import (IrrepsData, gate, index_add, radius_graph,
-                      spherical_harmonics)
+import e3nn_jax as e3nn
 from e3nn_jax.experimental.point_convolution import Convolution
 
 
@@ -23,14 +22,14 @@ def tetris():
 
     # Since chiral shapes are the mirror of one another we need an *odd* scalar to distinguish them
     labels = jnp.array([
-        [+1, 0, 0, 0, 0, 0, 0],  # chiral_shape_1
-        [-1, 0, 0, 0, 0, 0, 0],  # chiral_shape_2
-        [0, 1, 0, 0, 0, 0, 0],  # square
-        [0, 0, 1, 0, 0, 0, 0],  # line
-        [0, 0, 0, 1, 0, 0, 0],  # corner
-        [0, 0, 0, 0, 1, 0, 0],  # L
-        [0, 0, 0, 0, 0, 1, 0],  # T
-        [0, 0, 0, 0, 0, 0, 1],  # zigzag
+        [+1, 1, 0, 0, 0, 0, 0, 0],  # chiral_shape_1
+        [-1, 1, 0, 0, 0, 0, 0, 0],  # chiral_shape_2
+        [0, 0, 1, 0, 0, 0, 0, 0],  # square
+        [0, 0, 0, 1, 0, 0, 0, 0],  # line
+        [0, 0, 0, 0, 1, 0, 0, 0],  # corner
+        [0, 0, 0, 0, 0, 1, 0, 0],  # L
+        [0, 0, 0, 0, 0, 0, 1, 0],  # T
+        [0, 0, 0, 0, 0, 0, 0, 1],  # zigzag
     ], dtype=jnp.float32)
 
     pos = pos.reshape((8 * 4, 3))
@@ -42,8 +41,8 @@ def tetris():
 @hk.without_apply_rng
 @hk.transform
 def model(pos, edge_src, edge_dst):
-    node_feat = IrrepsData.ones("0e", (pos.shape[0],))
-    edge_attr = spherical_harmonics("0e + 1o + 2e", pos[edge_dst] - pos[edge_src], True, normalization='component')
+    node_feat = e3nn.IrrepsData.ones("0e", (pos.shape[0],))
+    edge_attr = e3nn.spherical_harmonics("0e + 1o + 2e", pos[edge_dst] - pos[edge_src], True, normalization='component')
 
     kw = dict(
         fc_neurons=None,
@@ -52,8 +51,8 @@ def model(pos, edge_src, edge_dst):
 
     for _ in range(4):
         node_feat = Convolution('32x0e + 32x0o + 16x0e + 8x1e + 8x1o', **kw)(node_feat, edge_src, edge_dst, edge_attr)
-        node_feat = jax.vmap(gate)(node_feat)
-    node_feat = Convolution('0o + 6x0e', **kw)(node_feat, edge_src, edge_dst, edge_attr)
+        node_feat = e3nn.gate(node_feat)
+    node_feat = Convolution('0o + 7x0e', **kw)(node_feat, edge_src, edge_dst, edge_attr)
 
     return node_feat.contiguous
 
@@ -63,23 +62,25 @@ def main():
 
     def loss_pred(params, pos, edge_src, edge_dst, labels, batch):
         pred = model.apply(params, pos, edge_src, edge_dst)
-        pred = index_add(batch, pred, 8)
-        loss = jnp.mean((pred - labels)**2)
+        pred = e3nn.index_add(batch, pred, 8)  # [batch, 1 + 7]
+        loss_odd = jnp.log(1 + jnp.exp(-labels[:, 0] * pred[:, 0]))
+        loss_even = jnp.mean(-labels[:, 1:] * jax.nn.log_softmax(pred[:, 1:]), axis=1)
+        loss = jnp.mean(loss_odd + loss_even)
         return loss, pred
 
     @jax.jit
     def update(params, opt_state, pos, edge_src, edge_dst, labels, batch):
         grad_fn = jax.value_and_grad(loss_pred, has_aux=True)
-        (loss, pred), grads = grad_fn(params, pos, edge_src, edge_dst, labels, batch)
-        accuracy = jnp.mean(jnp.all(jnp.round(pred) == labels, axis=1))
+        (_, pred), grads = grad_fn(params, pos, edge_src, edge_dst, labels, batch)
+        accuracy_odd = jnp.sign(jnp.round(pred[:, 0])) == labels[:, 0]
+        accuracy_even = jnp.argmax(pred[:, 1:], axis=1) == jnp.argmax(labels[:, 1:], axis=1)
+        accuracy = (jnp.mean(accuracy_odd) + jnp.mean(accuracy_even)) / 2
         updates, opt_state = opt.update(grads, opt_state)
         params = optax.apply_updates(params, updates)
-        return params, opt_state, loss, accuracy, pred
-
-    jnp.set_printoptions(precision=2, suppress=True)
+        return params, opt_state, accuracy, pred
 
     pos, labels, batch = tetris()
-    edge_src, edge_dst = radius_graph(pos, 1.1, batch)
+    edge_src, edge_dst = e3nn.radius_graph(pos, 1.1, batch)
 
     params = model.init(jax.random.PRNGKey(3), pos, edge_src, edge_dst)
     opt_state = opt.init(params)
@@ -87,14 +88,14 @@ def main():
     # compile jit
     wall = time.perf_counter()
     print("compiling...")
-    _, _, _, _, pred = update(params, opt_state, pos, edge_src, edge_dst, labels, batch)
+    _, _, _, pred = update(params, opt_state, pos, edge_src, edge_dst, labels, batch)
     pred.block_until_ready()
 
     print(f"It took {time.perf_counter() - wall:.1f}s to compile jit.")
 
     wall = time.perf_counter()
     for it in range(1, 2000):
-        params, opt_state, loss, accuracy, pred = update(params, opt_state, pos, edge_src, edge_dst, labels, batch)
+        params, opt_state, accuracy, pred = update(params, opt_state, pos, edge_src, edge_dst, labels, batch)
 
         print(f"[{it}] accuracy = {100 * accuracy:.0f}%")
 
@@ -102,6 +103,9 @@ def main():
             total = time.perf_counter() - wall
             print(f"100% accuracy has been reach in {total:.1f}s after {it} iterations ({1000 * total/it:.1f}ms/it).")
             break
+
+    jnp.set_printoptions(precision=2, suppress=True)
+    print(pred)
 
 
 if __name__ == '__main__':
