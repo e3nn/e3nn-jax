66d69a828d0e8129b8782a7d8cfbbd95085450a9
Mario Geiger <geiger.mario@gmail.com>
2022-04-07 14:38:19 -0400

    add loop to radius_graph

--- e3nn_jax/_graph_util.py
+++ e3nn_jax/_graph_util.py
@@ -28,7 +28,7 @@ def index_add(i, x, out_dim):
     return jnp.zeros((out_dim,) + x.shape[1:]).at[i].add(x)
 
 
-def radius_graph(pos, r_max, batch=None, size=None):
+def radius_graph(pos, r_max, *, batch=None, size=None, loop=False):
     r"""naive and inefficient version of ``torch_cluster.radius_graph``
 
     Args:
@@ -36,6 +36,7 @@ def radius_graph(pos, r_max, batch=None, size=None):
         r_max (float):
         batch (`jnp.ndarray`): indices
         size (int): size of the output
+        loop (bool): whether to include self-loops
 
     Returns:
         `jnp.ndarray`: src
@@ -45,11 +46,16 @@ def radius_graph(pos, r_max, batch=None, size=None):
         >>> key = jax.random.PRNGKey(0)
         >>> pos = jax.random.normal(key, (20, 3))
         >>> batch = jnp.arange(20) < 10
-        >>> radius_graph(pos, 0.8, batch)
+        >>> radius_graph(pos, 0.8, batch=batch)
         (DeviceArray([ 3,  7, 10, 11, 12, 18], dtype=int32), DeviceArray([ 7,  3, 11, 10, 18, 12], dtype=int32))
     """
     r = jax.vmap(jax.vmap(lambda x, y: jnp.linalg.norm(x - y), (None, 0), 0), (0, None), 0)(pos, pos)
-    src, dst = jnp.where((r < r_max) & (r > 0), size=size, fill_value=-1)
+    if loop:
+        mask = (r < r_max)
+    else:
+        mask = (r < r_max) & (r > 0)
+
+    src, dst = jnp.where(mask, size=size, fill_value=-1)
 
     if batch is None:
         return src, dst
