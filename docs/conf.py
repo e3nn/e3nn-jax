48a5d435865808c2f353e32c06ab20ebdea2e0a3
Mario Geiger <geiger.mario@gmail.com>
2022-07-30 23:16:42 -0400

    add docs

---
+++ docs/conf.py
@@ -0,0 +1,114 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+import os
+import sys
+from e3nn import __version__, __file__
+
+sys.path.insert(0, os.path.abspath("../"))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "e3nn-jax"
+copyright = "2022, e3nn Developers"
+author = "e3nn Developers"
+
+# The full version, including alpha/beta/rc tags
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "autodocsumm",
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "jupyter_sphinx",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pytorch": ("https://pytorch.org/docs/stable/", None),
+    "torch_geometric": ("https://pytorch-geometric.readthedocs.io/en/latest/", None),
+    "ase": ("https://wiki.fysik.dtu.dk/ase/", None),
+}
+
+autodoc_default_options = {
+    "inherited-members": False,
+    "show-inheritance": True,
+    "autosummary": False,
+}
+
+# The reST default role to use for all documents.
+default_role = "any"
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+
+myst_update_mathjax = False
+
+
+# Resolve function for the linkcode extension.
+# Thanks to https://github.com/Lasagne/Lasagne/blob/master/docs/conf.py
+def linkcode_resolve(domain, info):
+    def find_source():
+        # try to find the file and line number, based on code from numpy:
+        # https://github.com/numpy/numpy/blob/master/doc/source/conf.py#L286
+        obj = sys.modules[info["module"]]
+        for part in info["fullname"].split("."):
+            obj = getattr(obj, part)
+        import inspect
+        import os
+
+        fn = inspect.getsourcefile(obj)
+        fn = os.path.relpath(fn, start=os.path.dirname(__file__))
+        source, lineno = inspect.getsourcelines(obj)
+        return fn, lineno, lineno + len(source) - 1
+
+    if domain != "py" or not info["module"]:
+        return None
+
+    try:
+        rel_path, line_start, line_end = find_source()
+        # __file__ is imported from e3nn
+        filename = f"e3nn_jax/{rel_path}#L{line_start}-L{line_end}"
+    except Exception:
+        # no need to be relative to core here as module includes full path.
+        filename = info["module"].replace(".", "/") + ".py"
+
+    tag = __version__
+    return f"https://github.com/e3nn/e3nn-jax/blob/{tag}/{filename}"
---
+++ docs/index.rst
@@ -0,0 +1,85 @@
+Euclidean neural networks
+=========================
+
+What is ``e3nn-jax``?
+-----------------
+
+``e3nn-jax`` is a python library based on jax_ to create equivariant neural networks for the group :math:`O(3)`.
+
+
+Demonstration
+-------------
+
+If two tensors :math:`x` and :math:`y` transforms as :math:`D_x = 2 \times 1_o` (two vectors) and :math:`D_y = 0_e + 1_e` (a scalar and a pseudovector) respectively, where the indices :math:`e` and :math:`o` stand for even and odd -- the representation of parity,
+
+.. jupyter-execute::
+    :hide-code:
+
+    import jax
+    jax.numpy.set_printoptions(precision=3, suppress=True)
+
+.. jupyter-execute::
+
+    import jax
+    import e3nn_jax as e3nn
+
+    irreps_x = e3nn.Irreps("2x1o")
+    irreps_y = e3nn.Irreps("0e + 1e")
+
+    x = irreps_x.randn(jax.random.PRNGKey(0), (-1,))
+    y = irreps_y.randn(jax.random.PRNGKey(1), (-1,))
+
+    irreps_x.dim, irreps_y.dim
+
+
+their outer product is a :math:`6 \times 4` matrix of two indices :math:`A_{ij} = x_i y_j`.
+
+.. jupyter-execute::
+
+    A = jax.numpy.einsum("i,j", x, y)
+    A
+
+
+If a rotation is applied to the system, this matrix will transform with the representation :math:`D_x \otimes D_y` (the tensor product representation).
+
+.. math::
+
+    A = x y^t \longrightarrow A' = D_x A D_y^t
+
+Which can be represented by
+
+.. jupyter-execute::
+    :hide-code:
+
+    import matplotlib.pyplot as plt
+
+.. jupyter-execute::
+
+    R = e3nn.rand_matrix(jax.random.PRNGKey(2), ())
+    D_x = irreps_x.D_from_matrix(R)
+    D_y = irreps_y.D_from_matrix(R)
+
+    plt.imshow(jnp.kron(D_x, D_y), cmap="bwr", vmin=-1, vmax=1);
+
+
+This representation is not irreducible (is reducible). It can be decomposed into irreps by a change of basis. The outerproduct followed by the change of basis is done by the class `e3nn_jax.full_tensor_product`.
+
+.. jupyter-execute::
+
+    tp = e3nn.full_tensor_product(e3nn.IrrepsData.from_contiguous(irreps_x, x), e3nn.IrrepsData.from_contiguous(irreps_y, y))
+    tp
+
+
+As a sanity check, we can verify that the representation of the tensor prodcut is block diagonal and of the same dimension.
+
+.. jupyter-execute::
+
+    D = tp.irreps.D_from_matrix(R)
+    plt.imshow(D, cmap="bwr", vmin=-1, vmax=1);
+
+
+`e3nn_jax.full_tensor_product` is a special case of `e3nn_jax.FunctionalTensorProduct`, other ones like `e3nn_jax.FullyConnectedTensorProduct` can contained weights what can be learned, very useful to create neural networks.
+
+
+.. _jax: https://github.com/google/jax
+
