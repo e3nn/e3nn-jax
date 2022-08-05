df9d5d2ed1d2ef1a55e0202699ba3e365bf01085
Mario Geiger <geiger.mario@gmail.com>
2022-08-05 08:33:46 -0400

    add argument to index_add

--- e3nn_jax/_graph_util.py
+++ e3nn_jax/_graph_util.py
@@ -6,18 +6,41 @@ import jax.numpy as jnp
 import e3nn_jax as e3nn
 
 
-def index_add(indices: jnp.ndarray, input: Union[jnp.ndarray, e3nn.IrrepsArray], out_dim: int):
+def _distinct_but_small(x: jnp.ndarray) -> jnp.ndarray:
+    """Maps the input to the integers 0, 1, 2, ..., n-1, where n is the number of distinct elements in x.
+
+    Args:
+        x (``jnp.ndarray``): array of integers
+
+    Returns:
+        ``jnp.ndarray``: array of integers of same size
+    """
+    assert x.ndim == 1
+    unique = jnp.unique(x, size=x.shape[0])  # Pigeonhole principle
+    return jax.vmap(lambda i: jnp.where(i == unique, size=1)[0][0])(x)
+
+
+def index_add(
+    indices: jnp.ndarray, input: Union[jnp.ndarray, e3nn.IrrepsArray], *, out_dim: int = None, map_back: bool = False
+) -> Union[jnp.ndarray, e3nn.IrrepsArray]:
     r"""perform the operation
 
     ```
     out = zeros(out_dim, ...)
-    out[i] += x
+    out[indices] += input
+    ```
+
+    if ``map_back`` is ``True``, then the output is mapped back to the input position.
+
+    ```
+    return out[indices]
     ```
 
     Args:
         indices (``jnp.ndarray``): array of indices
         input (``jnp.ndarray`` or `e3nn_jax.IrrepsArray`): array of data
         out_dim (int): size of the output
+        map_back (bool): whether to map back to the input position
 
     Returns:
         ``jnp.ndarray`` or ``e3nn_jax.IrrepsArray``: output
@@ -28,12 +51,26 @@ def index_add(indices: jnp.ndarray, input: Union[jnp.ndarray, e3nn.IrrepsArray],
        >>> index_add(i, x, out_dim=4)
        DeviceArray([-9.,  0.,  5.,  0.], dtype=float32)
     """
+    assert indices.shape[0] == input.shape[0]
+
+    if out_dim is None and map_back is False:
+        # out_dim = jnp.max(indices) + 1
+        raise ValueError("out_dim must be specified if map_back is False")
+    if out_dim is not None and map_back is True:
+        raise ValueError("out_dim must not be specified if map_back is True")
+
+    if out_dim is None and map_back is True:
+        out_dim = indices.shape[0]
+        indices = _distinct_but_small(indices)
+
     x = input
     if isinstance(input, e3nn.IrrepsArray):
         x = input.array
 
-    # out_dim = jnp.max(i) + 1
     output = jnp.zeros((out_dim,) + x.shape[1:]).at[indices].add(x)
+
+    if map_back:
+        output = output[indices]
 
     if isinstance(input, e3nn.IrrepsArray):
         return e3nn.IrrepsArray(input.irreps, output)
