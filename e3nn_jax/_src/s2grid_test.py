615b8a4d2473edd41457ee8629643b98befc6ae2
Song Kim <songkimwl@gmail.com>
2022-10-25 15:27:27 -0400

    black formatting

--- e3nn_jax/_src/s2grid_test.py
+++ e3nn_jax/_src/s2grid_test.py
@@ -8,20 +8,20 @@ from e3nn_jax._src.spherical_harmonics import _legendre_spherical_harmonics
 
 key = jax.random.PRNGKey(0)
 
+
 def test_s2grid_transforms():
     res_alpha = 11
     res_beta = 8
     l = 3
 
-    c = jax.random.uniform(key, shape=(5, (l+1)**2))
+    c = jax.random.uniform(key, shape=(5, (l + 1) ** 2))
     res = to_s2grid(c, (res_beta, res_alpha))
     c_prime = from_s2grid(res, l)
     np.testing.assert_allclose(c, c_prime, rtol=1e-5, atol=1e-5)
 
 
 def test_fft():
-    res_alpha = 11
-    res_beta = 8
+    res_alpha = 11  # 2l+1
     l = 5
     x = jax.random.uniform(key, shape=(8, res_alpha))
     x_t = rfft(x, l)
