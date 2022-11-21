d29eb046627fb123240b86517e7cd8c02ac8af95
Mario Geiger <geiger.mario@gmail.com>
2022-11-21 16:46:22 -0500

    rename num_weights in num_indexed_weights in Linear

--- e3nn_jax/_src/linear.py
+++ e3nn_jax/_src/linear.py
@@ -183,11 +183,24 @@ class Linear(hk.Module):
     r"""Equivariant Linear Haiku Module.
 
     Args:
-        irreps_out (`Irreps`): output representations
+        irreps_out (`Irreps`): output representations.
         channel_out (optional int): if specified, the last axis before the irreps
             is assumed to be the channel axis and is mixed with the irreps.
+        irreps_in (optional `Irreps`): input representations. If not specified,
+            the input is obtained when calling the module.
+        biases (bool): whether to add a bias to the output.
+        path_normalization (str or float): Normalization of the paths, ``element`` or ``path``.
+            0/1 corresponds to a normalization where each element/path has an equal contribution to the forward.
+        gradient_normalization (str or float): Normalization of the gradients, ``element`` or ``path``.
+            0/1 corresponds to a normalization where each element/path has an equal contribution to the learning.
+        get_parameter (optional Callable): function to get the parameters.
+        num_indexed_weights (optional int): number of indexed weights. See example below.
+        weights_per_channel (bool): whether to have one set of weights per channel.
+        name (optional str): name of the module.
 
     Example:
+        Basic usage::
+
         >>> import e3nn_jax as e3nn
         >>> @hk.without_apply_rng
         ... @hk.transform
@@ -196,6 +209,34 @@ class Linear(hk.Module):
         >>> x = e3nn.IrrepsArray("1o + 2x0e", jnp.ones(5))
         >>> params = linear.init(jax.random.PRNGKey(0), x)
         >>> y = linear.apply(params, x)
+        >>> y.shape
+        (4,)
+
+        External weights::
+
+        >>> @hk.without_apply_rng
+        ... @hk.transform
+        ... def linear(w, x):
+        ...     return e3nn.Linear("0e + 1o")(w, x)
+        >>> x = e3nn.IrrepsArray("1o + 2x0e", jnp.ones(5))
+        >>> w = jnp.array([1., 2., 3., 4.])
+        >>> params = linear.init(jax.random.PRNGKey(0), w, x)
+        >>> y = linear.apply(params, w, x)
+        >>> y.shape
+        (4,)
+
+        External indices::
+
+        >>> @hk.without_apply_rng
+        ... @hk.transform
+        ... def linear(i, x):
+        ...     return e3nn.Linear("0e + 1o", num_indexed_weights=4)(i, x)
+        >>> x = e3nn.IrrepsArray("1o + 2x0e", jnp.ones((2, 5)))
+        >>> i = jnp.array([2, 3])
+        >>> params = linear.init(jax.random.PRNGKey(0), i, x)
+        >>> y = linear.apply(params, i, x)
+        >>> y.shape
+        (2, 4)
     """
 
     def __init__(
@@ -208,7 +249,7 @@ class Linear(hk.Module):
         path_normalization: Union[str, float] = None,
         gradient_normalization: Union[str, float] = None,
         get_parameter: Optional[Callable[[str, Instruction], jnp.ndarray]] = None,
-        num_weights: Optional[int] = None,
+        num_indexed_weights: Optional[int] = None,
         weights_per_channel: bool = False,
         name: Optional[str] = None,
     ):
@@ -220,7 +261,7 @@ class Linear(hk.Module):
         self.instructions = None
         self.biases = biases
         self.path_normalization = path_normalization
-        self.num_weights = num_weights
+        self.num_indexed_weights = num_indexed_weights
         self.weights_per_channel = weights_per_channel
 
         if gradient_normalization is None:
@@ -245,7 +286,7 @@ class Linear(hk.Module):
 
         Args:
             weights (optional IrrepsArray or jnp.ndarray): scalar weights that are contracted with free parameters.
-                An array of shape ``(..., num_weights)``. Broadcasting with `input` is supported.
+                An array of shape ``(..., contracted_axis)``. Broadcasting with `input` is supported.
             input (IrrepsArray): input irreps-array of shape ``(..., [channel_in,] irreps_in.dim)``.
                 Broadcasting with `weights` is supported.
 
@@ -258,7 +299,10 @@ class Linear(hk.Module):
             weights = None
 
         if self.irreps_in is not None:
-            input = input._convert(self.irreps_in)
+            if self.irreps_in.regroup() != input.irreps.regroup():
+                raise ValueError(
+                    f"e3nn.Linear: The input irreps ({input.irreps}) do not match the expected irreps ({self.irreps_in})"
+                )
 
         input = input.remove_nones().regroup()
         output_irreps = self.irreps_out.simplify()
@@ -299,7 +343,7 @@ class Linear(hk.Module):
                     raise ValueError("weights must be scalar")
                 weights = weights.array
 
-            if weights.dtype.kind == "i" and self.num_weights is not None:
+            if weights.dtype.kind == "i" and self.num_indexed_weights is not None:
                 assert not self.weights_per_channel  # Not implemented yet
 
                 shape = jnp.broadcast_shapes(input.shape[:-1], weights.shape)
@@ -308,12 +352,14 @@ class Linear(hk.Module):
 
                 w = [
                     self.get_parameter(
-                        f"b[{ins.i_out}] {lin.irreps_out[ins.i_out]}", (self.num_weights,) + ins.path_shape, ins.weight_std
+                        f"b[{ins.i_out}] {lin.irreps_out[ins.i_out]}",
+                        (self.num_indexed_weights,) + ins.path_shape,
+                        ins.weight_std,
                     )
                     if ins.i_in == -1
                     else self.get_parameter(
                         f"w[{ins.i_in},{ins.i_out}] {lin.irreps_in[ins.i_in]},{lin.irreps_out[ins.i_out]}",
-                        (self.num_weights,) + ins.path_shape,
+                        (self.num_indexed_weights,) + ins.path_shape,
                         ins.weight_std,
                     )
                     for ins in lin.instructions
@@ -325,7 +371,7 @@ class Linear(hk.Module):
                     f = jax.vmap(f)
                 output = f(w, input)
 
-            elif weights.dtype.kind in "fc" and self.num_weights is None:
+            elif weights.dtype.kind in "fc" and self.num_indexed_weights is None:
 
                 if self.weights_per_channel:
                     shape = jnp.broadcast_shapes(input.shape[:-2], weights.shape[:-1])
