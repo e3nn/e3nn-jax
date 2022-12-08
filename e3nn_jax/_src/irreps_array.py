0a2393e3f78a37f741f075f05b7349d426fa15a9
Mario Geiger <geiger.mario@gmail.com>
2022-12-08 16:59:25 -0500

    IrrepsArray: simpler pytree structure

--- e3nn_jax/_src/irreps_array.py
+++ e3nn_jax/_src/irreps_array.py
@@ -874,8 +874,8 @@ class IrrepsArray:
 
 jax.tree_util.register_pytree_node(
     IrrepsArray,
-    lambda x: ((x.array, x.list), x.irreps),
-    lambda x, data: IrrepsArray(irreps=x, array=data[0], list=data[1], _perform_checks=False),
+    lambda x: (x.array, x.irreps),
+    lambda irreps, array: IrrepsArray(irreps, array, _perform_checks=False),
 )
 
 
