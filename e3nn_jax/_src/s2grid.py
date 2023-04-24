1680fcff5dc0581b7fde33ca2c5c2d45bfb7af24
Mario Geiger <geiger.mario@gmail.com>
2023-04-24 17:47:57 +0200

    quadrature: str = "gausslegendre"

--- e3nn_jax/_src/s2grid.py
+++ e3nn_jax/_src/s2grid.py
@@ -705,7 +705,7 @@ def to_s2grid(
     res_beta: int,
     res_alpha: int,
     *,
-    quadrature: str,
+    quadrature: str = "gausslegendre",
     normalization: str = "integral",
     fft: bool = True,
     p_val: Optional[int] = None,
