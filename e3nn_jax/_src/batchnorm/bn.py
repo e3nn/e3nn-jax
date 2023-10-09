57e5500c8f476f856476ac8707b6664e9980940f
Mario Geiger <geiger.mario@gmail.com>
2023-10-09 10:31:52 +0200

    move BN in a folder

--- e3nn_jax/_src/batchnorm/bn.py
+++ e3nn_jax/_src/batchnorm/bn.py
@@ -1,20 +1,19 @@
 from math import prod
 
-import haiku as hk
 import jax
 import jax.numpy as jnp
 
 import e3nn_jax as e3nn
 
 
-def _batch_norm(
-    input,
+def batch_norm(
+    input: e3nn.IrrepsArray,
     running_mean,
     running_var,
     weight,
     bias,
     normalization,
-    reduce,
+    reduce: str,
     is_training,
     is_instance,
     has_affine,
@@ -121,118 +120,3 @@ def _batch_norm(
     output = e3nn.from_chunks(input.irreps, fields, (batch, prod(size)), input.dtype)
     output = output.reshape((batch,) + tuple(size) + (-1,))
     return output, new_means, new_vars
-
-
-class BatchNorm(hk.Module):
-    """Equivariant Batch Normalization.
-
-    It normalizes by the norm of the representations.
-    Note that the norm is invariant only for orthonormal representations.
-    Irreducible representations are orthonormal.
-
-    Args:
-        irreps: Irreducible representations of the input and output (unchanged)
-        eps (float): epsilon for numerical stability, has to be between 0 and 1.
-            the field norm is transformed to ``(1 - eps) * norm + eps``
-            leading to a slower convergence toward norm 1.
-        momentum: momentum for moving average
-        affine: whether to include learnable biases
-        reduce: reduce mode, either 'mean' or 'max'
-        instance: whether to use instance normalization
-        normalization: normalization mode, either 'norm' or 'component'
-    """
-
-    def __init__(
-        self,
-        *,
-        irreps: e3nn.Irreps = None,
-        eps: float = 1e-4,
-        momentum: float = 0.1,
-        affine: bool = True,
-        reduce: str = "mean",
-        instance: bool = False,
-        normalization: str = None,
-    ):
-        super().__init__()
-
-        # TODO test with and without irreps argument given
-
-        self.irreps = e3nn.Irreps(irreps) if irreps is not None else irreps
-        self.eps = eps
-        self.momentum = momentum
-        self.affine = affine
-        self.instance = instance
-
-        assert isinstance(reduce, str), "reduce should be passed as a string value"
-        assert reduce in ["mean", "max"], "reduce needs to be 'mean' or 'max'"
-        self.reduce = reduce
-
-        if normalization is None:
-            normalization = e3nn.config("irrep_normalization")
-        assert normalization in [
-            "norm",
-            "component",
-        ], "normalization needs to be 'norm' or 'component'"
-        self.normalization = normalization
-
-    def __repr__(self):
-        return f"{self.__class__.__name__} ({self.irreps}, eps={self.eps}, momentum={self.momentum})"
-
-    def __call__(
-        self, input: e3nn.IrrepsArray, is_training: bool = True
-    ) -> e3nn.IrrepsArray:
-        r"""Evaluate the batch normalization.
-
-        Args:
-            input: input tensor of shape ``(batch, [spatial], irreps.dim)``
-            is_training: whether to train or evaluate
-
-        Returns:
-            output: normalized tensor of shape ``(batch, [spatial], irreps.dim)``
-        """
-        if self.irreps is not None:
-            input = input.rechunk(self.irreps)
-
-        num_scalar = sum(mul for mul, ir in input.irreps if ir.is_scalar())
-        num_features = input.irreps.num_irreps
-
-        if not self.instance:
-            running_mean = hk.get_state(
-                "running_mean", shape=(num_scalar,), init=jnp.zeros
-            )
-            running_var = hk.get_state(
-                "running_var", shape=(num_features,), init=jnp.ones
-            )
-        else:
-            running_mean = None
-            running_var = None
-
-        if self.affine:
-            weight = hk.get_parameter("weight", shape=(num_features,), init=jnp.ones)
-            bias = hk.get_parameter("bias", shape=(num_scalar,), init=jnp.zeros)
-        else:
-            weight = None
-            bias = None
-
-        output, new_means, new_vars = _batch_norm(
-            input,
-            running_mean=running_mean,
-            running_var=running_var,
-            weight=weight,
-            bias=bias,
-            normalization=self.normalization,
-            reduce=self.reduce,
-            is_training=is_training,
-            is_instance=self.instance,
-            has_affine=self.affine,
-            momentum=self.momentum,
-            epsilon=self.eps,
-        )
-
-        if is_training and not self.instance:
-            if len(new_means):
-                hk.set_state("running_mean", jnp.concatenate(new_means))
-            if len(new_vars):
-                hk.set_state("running_var", jnp.concatenate(new_vars))
-
-        return output
--- e3nn_jax/haiku.py
+++ e3nn_jax/haiku.py
@@ -8,7 +8,7 @@ else:
     from e3nn_jax._src.linear_haiku import Linear
     from e3nn_jax._src.mlp_haiku import MultiLayerPerceptron
     from e3nn_jax._src.fc_tp_haiku import FullyConnectedTensorProduct
-    from e3nn_jax._src.batchnorm_haiku import BatchNorm
+    from e3nn_jax._src.batchnorm.bn_haiku import BatchNorm
     from e3nn_jax._src.dropout_haiku import Dropout
     from e3nn_jax._src.symmetric_tensor_product_haiku import SymmetricTensorProduct
 
