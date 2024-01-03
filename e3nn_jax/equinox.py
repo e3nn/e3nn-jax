9f732cd6ef86eb47e413affc5b377b84fc07afe6
Ameya Daigavane <ameya.d.98@gmail.com>
2024-01-03 18:57:38 +0530

    Fix imports.

--- e3nn_jax/equinox.py
+++ e3nn_jax/equinox.py
@@ -1,3 +1,10 @@
-from e3nn_jax._src.linear_equinox import Linear
+try:
+    import equinox
 
-__all__ = ["Linear"]
+    del equinox
+except ImportError:
+    pass
+else:
+    from e3nn_jax._src.linear_equinox import Linear
+
+    __all__ = ["Linear"]
