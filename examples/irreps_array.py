0297e6894c637b964d8f34f5f65bcc47f86c3a93
Ameya Daigavane <ameya.d.98@gmail.com>
2026-04-01 15:16:54 -0400

    Formatting.

--- examples/irreps_array.py
+++ examples/irreps_array.py
@@ -2,7 +2,6 @@ import jax.numpy as jnp
 
 import e3nn_jax as e3nn
 
-
 x1 = e3nn.IrrepsArray("0e + 1o", jnp.array([1.0, 2.0, 3.0, 4.0]))
 x2 = e3nn.IrrepsArray("2e", jnp.array([1.0, 2.0, 3.0, 4.0, 5.0]))
 
